28514c64924b91d536accd29aa0a91335c8365e2
jsots <justinsotolongo@gmail.com>
2023-05-25 11:36:22 -0400

    Longest Common Prefix

---
+++ leetcode/Easy/romanToInt.js
@@ -0,0 +1,23 @@
+symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+let value = 0
+
+let romanToInt = function(s) {
+    value = 0;
+    for(let i = 0; i < s.length; i+=1) {
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    }
+    return value
+}
+
+console.log(romanToInt("III"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("MCMXCIV"))
