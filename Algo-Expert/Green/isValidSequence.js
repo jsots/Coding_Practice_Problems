b93b4bad6d753352e7fd3ba4353ca0a5295c07ab
jsots <justinsotolongo@gmail.com>
2023-05-25 12:21:27 -0400

    Is Valid Sequence fixed

--- Algo-Expert/Green/isValidSequence.js
+++ Algo-Expert/Green/isValidSequence.js
@@ -1,18 +1,15 @@
-function isValidSequence(arr, seq) {
-    let c = 0
-    for (let i=0; i < arr.length; i++) {
-      if (seq.includes(arr[i])) {
-        c++
-      }
-    }
-    if (c === seq.length) {
-      return true
-    } else {
-      return false
-    }
+function isValidSubsequence(arr, seq) {
+  // Write your code here.
+  let arrIdx = 0;
+  let seqIdx = 0;
+  while (arrIdx < arr.length && seqIdx < seq.length) {
+    if (arr[arrIdx] === seq[seqIdx]) seqIdx ++;
+    arrIdx++
   }
+  return seqIdx === seq.length;
+}
   
   let array2 = [5, 1, 22, 25, 6, -1, 8, 10]
   let sequence = [1, 6, -1, 10]
   
-  console.log(isValidSequence(array2, sequence))
+  console.log(isValidSubsequence(array2, sequence))
