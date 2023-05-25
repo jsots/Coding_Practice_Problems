491ae70a30bab61ae99772077b2cc72cd2ead022
jsots <justinsotolongo@gmail.com>
2023-05-25 11:42:20 -0400

    Fixed two Number Sum problem

--- Algo-Expert/Green/twoNumberSum.js
+++ Algo-Expert/Green/twoNumberSum.js
@@ -1,16 +1,16 @@
-function twoSum(arr, int) {
+function twoNumberSum(arr, int) {
     let ans = []
-    for (let i=0; i<arr.length-1; i++) {
-      for (let j=0; j<arr.length-1; j++) {
+    for (let i=0; i<arr.length; i++) {
+      for (let j=0; j<arr.length; j++) {
         if(i!==j && arr[i]+arr[j] === int) {
           ans.push(arr[i])
         }
       }
     }
     return ans
-  }
+}
   
   let array = [3, 5, -4, 8, 11, 1, -1, 6]
   let targetSum = 10
   
-  console.log(twoSum(array, targetSum))
+  console.log(twoNumberSum(array, targetSum))
