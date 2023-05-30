abe51d0c203dfd24ebf165864f991415692c9b0d
jsots <justinsotolongo@gmail.com>
2023-05-30 17:43:15 -0400

    Valid Palindrom completed but taken from leetcode solutions. need to understand it better

--- leetcode/Easy/validPalindrome.js
+++ leetcode/Easy/validPalindrome.js
@@ -1,46 +1,21 @@
-let validPalindrome = function (s) {
-    let alphabet = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0
-    }
-    for (let i=0; i < s.length; i++) {
-        if (alphabet.hasOwnProperty(s.charAt(i))) {
-            alphabet[s.charAt(i)] += 1
+var isPalindrome = function(s){
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        if (!isAlphaNumeric(s[left]))
+            left++;
+        else if (!isAlphaNumeric(s[right]))
+            right--;
+        else if (s[left].toLowerCase() !== s[right].toLowerCase())
+            return false;
+        else {
+            left++;
+            right--;
         }
     }
-    console.log(alphabet)
-    const sumValues = alphabet => Object.values(alphabet).reduce((a, b) => a + b, 0);
-    console.log(sumValues)
-    if (sumValues % 2 === 0) {
-        return false
-    } else {
-        return true
-    }
+    return true;
 }
 
-s = "race a car"
-console.log(validPalindrome(s))
+function isAlphaNumeric(char) {
+    const code = char.charCodeAt(0);
+    return (code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
