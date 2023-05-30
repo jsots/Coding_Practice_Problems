50022e75dd113d865858dd6d377d87205fa2e92e
jsots <justinsotolongo@gmail.com>
2023-05-30 16:46:46 -0400

    Completed find length of lcis, but my solution isnt even close to optimal LOL

--- Algo-Expert/Green/findLengthOfLCIS.js
+++ Algo-Expert/Green/findLengthOfLCIS.js
@@ -1,15 +1,25 @@
 let findLengthOfLCIS = function(nums) {
     let lCIS = []
+    let ans = 1
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] > nums[i-1]) {
             if (lCIS.length === 0) {
                 lCIS.push(nums[i-1])
             }
             lCIS.push(nums[i])
+        } else {
+            if (lCIS.length > ans) {
+                ans = lCIS.length
+            }
+            lCIS = []
         }
     }
-    return lCIS
+    if (lCIS.length > ans) {
+        return lCIS.length
+    } else {
+        return ans
+    }
 }
 
-nums = [1,3,5,4,7]
+nums = [1,3,5,4, 7]
 console.log(findLengthOfLCIS(nums))
