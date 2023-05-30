66879893e609acf926efae18e83b5c8b0537cf65
jsots <justinsotolongo@gmail.com>
2023-05-30 13:31:29 -0400

    Fixed sorted squred array

--- Algo-Expert/Green/sortedSquaredArray.js
+++ Algo-Expert/Green/sortedSquaredArray.js
@@ -1,14 +1,14 @@
 // Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order. 
-
-function sortedSquaredArray (arr) {
-    let sqrs = []
-    let sqr = 0
-    arr.forEach((num) => {
+function sortedSquaredArray(array) {
+  // Write your code here.
+  let sqrs = []
+  let sqr = 0
+    array.forEach((num) => {
       sqr = num**2
       sqrs.push(sqr)
     })
-    return sqrs
-  }
-  
-  let array3 = [1, 2, 3, 5, 6, 8, 9]
-  console.log(sortedSquaredArray(array3))
+  return sqrs.sort(function(a, b){return a - b})
+}
+
+let array3 = [1, 2, 3, 5, 6, 8, 9]
+console.log(sortedSquaredArray(array3))
