44c8248975f3878e634f387b7cc234dc0f098c79
jsots <justinsotolongo@gmail.com>
2023-05-30 17:57:28 -0400

    Note the alpha numeric function

--- leetcode/Easy/validPalindrome.js
+++ leetcode/Easy/validPalindrome.js
@@ -19,3 +19,5 @@ function isAlphaNumeric(char) {
     const code = char.charCodeAt(0);
     return (code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
 }
+
+// The alphanumeric function might come in handy later.
