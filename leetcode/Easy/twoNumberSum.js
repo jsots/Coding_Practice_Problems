90934e2eba8af0d14bfba068a63d4d97d27c2d9f
jsots <justinsotolongo@gmail.com>
2023-05-31 17:55:35 -0400

    Fixed twoNumberSum

--- leetcode/Easy/twoNumberSum.js
+++ leetcode/Easy/twoNumberSum.js
@@ -1,9 +1,11 @@
 let twoSum = function(nums, target) {
     let ans = []
-    for (let i=0; i<nums.length; i++) {
-      for (let j=0; j<nums.length; j++) {
-        if(i!==j && nums[i]+nums[j] === target) {
+    let len = nums.length
+    for (let i=0; i<len; i++) {
+      for (let j=i+1; j<len; j++) {
+        if(nums[i]+nums[j] === target) {
           ans.push(i)
+          ans.push(j)
         }
       }
     }
