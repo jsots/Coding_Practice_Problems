f1bc443d3bf6858f63dbbf8a8b5f7c56a29be88e
jsots <justinsotolongo@gmail.com>
2023-05-31 17:46:01 -0400

    both solutions here

--- leetcode/Easy/validParenthesis.js
+++ leetcode/Easy/validParenthesis.js
@@ -35,3 +35,42 @@ let isValid = function(s) {
     }
     return true
 };
+
+// Below is the most optimal solution in terms of time and space. Specifically time.
+
+let isValid2 = function(s) {
+    let stack = []
+    if (s.length % 2 !== 0) {
+        return false
+    }
+    for (let i = 0; i < s.length; i++) {
+        let current = s.charAt(i)
+        if (current === "(" || current === "{" || current === "[") {
+            stack.push(current)
+        } else if (current === ")") {
+            if (stack[stack.length-1] === "(") {
+                stack.pop()
+            } else {
+                return false
+            }
+        } else if (current === "]") {
+            if (stack[stack.length-1] === "[") {
+                stack.pop()
+            } else {
+                return false
+            }
+        } else if (current === "}") {
+            if (stack[stack.length-1] === "{") {
+                stack.pop()
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    if (stack.length !== 0) {
+        return false
+    }
+    return true
+};
