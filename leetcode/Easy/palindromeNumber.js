c9a5f2d3caf61663b9cc22d609d14a730c99a316
jsots <justinsotolongo@gmail.com>
2023-05-31 11:24:19 -0400

    Is number palindrome

---
+++ leetcode/Easy/palindromeNumber.js
@@ -0,0 +1,16 @@
+let isPalindrom = function(x) {
+    let left = 0
+    let s = x.toString()
+    let right = s.length -1
+    while (left > right) {
+        if (s.charAt(left) === s.charAt(right)) {
+            left ++
+            right --
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPalindrom(121))
