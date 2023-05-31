f7d5631690fcc03cb5111fed97bfca02ac02cdee
jsots <justinsotolongo@gmail.com>
2023-05-31 17:58:29 -0400

    Cleaned up the efficiency for twoSumNum problem

--- leetcode/Easy/twoNumberSum.js
+++ leetcode/Easy/twoNumberSum.js
@@ -1,11 +1,11 @@
 let twoSum = function(nums, target) {
     let ans = []
-    let len = nums.length
-    for (let i=0; i<len; i++) {
-      for (let j=i+1; j<len; j++) {
+    for (let i=0; i<nums.length; i++) {
+      for (let j=i+1; j<nums.length; j++) {
         if(nums[i]+nums[j] === target) {
           ans.push(i)
           ans.push(j)
+          break
         }
       }
     }
