45fd56851e9b2192c37d9179de030519440bca4f
jsots <justinsotolongo@gmail.com>
2023-06-01 10:35:54 -0400

    Third attempt, the worst so far. need to mutate original array

--- leetcode/Easy/removeDuplicates.js
+++ leetcode/Easy/removeDuplicates.js
@@ -2,12 +2,11 @@ var removeDuplicates = function(nums) {
     let indeces = []
     for (let i=0; i<nums.length; i++) {
         if (nums[i] === nums[i+1]) {
-            indeces.push(i)
+            indeces.push(nums[i])
+            i++
         }
+        indeces.push(nums[i])
     }
-    for (let j = 0; j<indeces.length; j++) {
-        nums.splice(indeces[j], indeces[j]+1)
-    }
-    let k = nums.length
+    let k = indeces.length
     return k
 };
