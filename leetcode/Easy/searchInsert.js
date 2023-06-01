2c07af655001aee21ee25e5fd8a002c61df2e0f0
jsots <justinsotolongo@gmail.com>
2023-06-01 12:43:20 -0400

    Optimized more

--- leetcode/Easy/searchInsert.js
+++ leetcode/Easy/searchInsert.js
@@ -1,3 +1,5 @@
+// 31.67% runtime & 20.18% memory
+
 var searchInsert = function(nums, target) {
     if (nums.includes(target)) {
         return nums.indexOf(target)
@@ -5,6 +7,26 @@ var searchInsert = function(nums, target) {
         return 0
     } else if (target > nums[nums.length-1]) {
         return nums.length
+    }
+    for (let i=0; i<nums.length-1; i++) {
+        if (target > nums [i] && target < nums[i+1]) {
+            nums.splice(i, 0, target)
+            return i+1
+        }
+    }
+};
+
+// 60.69% runtime & 82.46% memory
+
+var searchInsert = function(nums, target) {
+    let lastIndex = nums.length-1
+    let len = nums.length
+    if (nums.includes(target)) {
+        return nums.indexOf(target)
+    } else if (target < nums[0]) {
+        return 0
+    } else if (target > nums[lastIndex]) {
+        return len
     }
     for (let i=0; i<nums.length-1; i++) {
         if (target > nums [i] && target < nums[i+1]) {
