6966ae82a01db9c39b8e2e42a517f9add822e07b
jsots <justinsotolongo@gmail.com>
2023-06-01 12:18:56 -0400

    Improved version included for strStr

--- leetcode/Easy/strStr.js
+++ leetcode/Easy/strStr.js
@@ -1,3 +1,4 @@
+// Below is 90.1% runtime and 31.74% memory.
 var strStr = function(haystack, needle) {
     if (haystack.includes(needle)) {
         let ans = haystack.indexOf(needle);
@@ -7,6 +8,11 @@ var strStr = function(haystack, needle) {
 };
 
 
-let haystack = "sadbutsad" 
-let needle = "sad"
-console.log(strStr(haystack, needle))
+// Below is 70.54% runtime & 75.35% memory
+var strStr2 = function(haystack, needle) {
+    if (!haystack.includes(needle)) {
+        return -1
+    }
+    let ans = haystack.indexOf(needle);
+    return ans;
+};
