85bf087ee50b6baf4eb48229055af0793f64c991
jsots <justinsotolongo@gmail.com>
2023-06-01 13:00:46 -0400

    improved

--- leetcode/Easy/lengthOfLastWord.js
+++ leetcode/Easy/lengthOfLastWord.js
@@ -1,6 +1,7 @@
 var lengthOfLastWord = function(s) {
     let ans = s.split(" ")
-    for (let i=ans.length-1; i>=0; i--) {
+    let lastIndex = ans.length-1
+    for (let i=lastIndex; i>=0; i--) {
         if (ans[i] !== "") {
             return ans[i].length
         }
