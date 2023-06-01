4a84e7da9ef04af24afe1baa09d7dd1ff39088af
jsots <justinsotolongo@gmail.com>
2023-06-01 12:11:38 -0400

    Compelted with chat gpt help

--- leetcode/Easy/strStr.js
+++ leetcode/Easy/strStr.js
@@ -1,11 +1,7 @@
 var strStr = function(haystack, needle) {
-    if (haystack.includes(`${needle}`)) {
-        let ans = haystack.split(`${needle}`)
-        for(let i= 0; i<ans.length; i++) {
-            if (ans[i] === "") {
-                return i
-            }
-        }
+    if (haystack.includes(needle)) {
+        let ans = haystack.indexOf(needle);
+        return ans;
     }
     return -1
 };
