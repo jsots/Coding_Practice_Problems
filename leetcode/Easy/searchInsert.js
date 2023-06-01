970432eeb84303e8fc3fa15c5d02e346a0f31ca4
jsots <justinsotolongo@gmail.com>
2023-06-01 12:47:17 -0400

    Added another solution with different but good stats

--- leetcode/Easy/searchInsert.js
+++ leetcode/Easy/searchInsert.js
@@ -18,7 +18,7 @@ var searchInsert = function(nums, target) {
 
 // 60.69% runtime & 82.46% memory
 
-var searchInsert = function(nums, target) {
+var searchInsert2 = function(nums, target) {
     let lastIndex = nums.length-1
     let len = nums.length
     if (nums.includes(target)) {
@@ -27,6 +27,26 @@ var searchInsert = function(nums, target) {
         return 0
     } else if (target > nums[lastIndex]) {
         return len
+    }
+    for (let i=0; i<nums.length-1; i++) {
+        if (target > nums [i] && target < nums[i+1]) {
+            nums.splice(i, 0, target)
+            return i+1
+        }
+    }
+};
+
+// 76.47% runtime & 66.27% memory
+
+var searchInsert3 = function(nums, target) {
+    let lastIndex = nums.length-1
+    let len = nums.length
+    if (target < nums[0]) {
+        return 0
+    } else if (target > nums[lastIndex]) {
+        return len
+    } else if (nums.includes(target)) {
+        return nums.indexOf(target)
     }
     for (let i=0; i<nums.length-1; i++) {
         if (target > nums [i] && target < nums[i+1]) {
