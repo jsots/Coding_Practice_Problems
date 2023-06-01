8d45c84f009dbb62fd1ea8e9abdc02c077631359
jsots <justinsotolongo@gmail.com>
2023-06-01 10:56:19 -0400

    Included the solution from leetcode that has better stats

--- leetcode/Easy/removeDuplicates.js
+++ leetcode/Easy/removeDuplicates.js
@@ -8,3 +8,16 @@ var removeDuplicates = function(nums) {
     let k = nums.length
     return k
 };
+
+// Solution below has a better run time and memory. 
+
+var removeDuplicates2 = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] !== nums[j]) {
+        i++;
+        nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+  };
