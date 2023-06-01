f1649cc1a1a1a886561abac3c0843e8ef67622bf
jsots <justinsotolongo@gmail.com>
2023-06-01 11:45:53 -0400

    improved the memory at the cost of time in second attempt

--- leetcode/Easy/removeElement.js
+++ leetcode/Easy/removeElement.js
@@ -12,3 +12,18 @@ var removeElement = function(nums, val) {
     }
     return k;
 };
+
+// Below solution improves the memory but worsens the run time. run becomes 25.83 and memory becomes 91.61
+
+var removeElement2 = function(nums, val) {
+    let ans = []
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] !== val) {
+          ans.push(nums[j])
+        } 
+    }
+    for (let i = 0; i<ans.length; i++) {
+        nums[i] = ans[i]
+    }
+    return ans.length;
+};
