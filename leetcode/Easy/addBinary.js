99e7b0f220638bb9691e12b2a55dfb8c78c4644d
jsots <justinsotolongo@gmail.com>
2023-06-02 12:49:57 -0400

    Add Binary, but chat fixed my answer

---
+++ leetcode/Easy/addBinary.js
@@ -0,0 +1,30 @@
+var addBinary = function(a, b) {
+    let ans = [];
+    let carry = 0;
+    let output = "";
+  
+    // Pad the shorter binary string with leading zeros
+    if (a.length < b.length) {
+      a = a.padStart(b.length, "0");
+    } else {
+      b = b.padStart(a.length, "0");
+    }
+  
+    // Perform binary addition
+    for (let i = a.length - 1; i >= 0; i--) {
+      let sum = Number(a[i]) + Number(b[i]) + carry;
+      ans.push(sum % 2);
+      carry = Math.floor(sum / 2);
+    }
+  
+    // If there's still a carry at the end, add it to the result
+    if (carry) {
+      ans.push(carry);
+    }
+  
+    // Reverse the result array and convert it to a string
+    output = ans.reverse().join("");
+  
+    return output;
+  };
+  
