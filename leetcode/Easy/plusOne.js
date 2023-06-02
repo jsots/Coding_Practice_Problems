ef67ed4cd374b2d1423bd308f3e1e588dcef790b
jsots <justinsotolongo@gmail.com>
2023-06-02 10:43:41 -0400

    Completed plus one, need to optimize

--- leetcode/Easy/plusOne.js
+++ leetcode/Easy/plusOne.js
@@ -1,17 +1,17 @@
 var plusOne = function(digits) {
     let finalIndex = digits.length-1
     for (let i = finalIndex; i >= 0; i--) {
-        let plusOneTarget = digits[i]
-        if (plusOneTarget < 9) {
-            plusOneTarget += 1
+        if (digits[i] < 9) {
+            digits[i] += 1
             return digits
         } else if (i === 0) {
+            digits[i] = 0
             digits.unshift(1)
-            plusOneTarget = 0
         } else {
-            plusOneTarget = 0
+            digits[i] = 0
         }
     }
     return digits
 };
 
+console.log(plusOne([1,2,3]))
