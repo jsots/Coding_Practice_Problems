2ddfd62feaf2e6ec38f97b72bfaa4e2a2ae433a5
jsots <justinsotolongo@gmail.com>
2023-06-02 10:45:52 -0400

    better run time but still bad in memory

--- leetcode/Easy/plusOne.js
+++ leetcode/Easy/plusOne.js
@@ -1,6 +1,5 @@
 var plusOne = function(digits) {
-    let finalIndex = digits.length-1
-    for (let i = finalIndex; i >= 0; i--) {
+    for (let i = digits.length-1; i >= 0; i--) {
         if (digits[i] < 9) {
             digits[i] += 1
             return digits
