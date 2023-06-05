8e4528478318d52df20247f34ad7f3b32a2338f9
jsots <justinsotolongo@gmail.com>
2023-06-05 16:44:30 -0400

    included a leetcode solution that is basically optimal

--- leetcode/Easy/validAnagram.js
+++ leetcode/Easy/validAnagram.js
@@ -8,3 +8,24 @@ var isAnagram = function(s, t) {
     }
     return false
 };
+
+// Runtime - 99.71% Memory - 92.29%
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length){
+        return false;
+    }
+
+    const freq = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        const charCodeS = s.charCodeAt(i) - 97;
+        const charCodeT = t.charCodeAt(i) - 97;
+
+        freq[charCodeS]++;
+        freq[charCodeT]--;
+    }
+
+    return freq.every((value) => value === 0);
+
+}
