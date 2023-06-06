447d2d4a630ca20d60d2ba4c7d78084fedafba2b
jsots <justinsotolongo@gmail.com>
2023-06-06 13:28:50 -0400

    Included a much faster run time version

--- leetcode/Easy/mySqrt.js
+++ leetcode/Easy/mySqrt.js
@@ -11,4 +11,28 @@ var mySqrt = function(x) {
     return i-1
 };
 
-// 
+// Runtime 98.55% and Memory 13.44%
+
+var mySqrt = function(x) {
+    if (x === 0) {
+        return 0;
+    }
+
+    let left = 1;
+    let right = x;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let sqr = mid * mid;
+
+        if (sqr === x) {
+            return mid;
+        } else if (sqr < x) {
+            left = mid +1;
+        } else {
+            right = mid -1;
+        }
+    }
+
+    return right
+};
