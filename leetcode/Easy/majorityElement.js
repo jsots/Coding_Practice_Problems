d54a46d5c1eaf1a1796a1c9f923fd51e77227e82
jsots <justinsotolongo@gmail.com>
2023-06-08 11:13:41 -0400

    Added a new solution that is different... and kinda better

--- leetcode/Easy/majorityElement.js
+++ leetcode/Easy/majorityElement.js
@@ -15,3 +15,27 @@ var majorityElement = function(nums) {
     }
     return majorityElement
 };
+
+// Runtime - 47.82% and Memory = 24.73%
+
+var majorityElement = function(nums) {
+    let counter = 0
+    let maxCounter = 0
+    let maxNumber = 0
+    nums.sort()
+    if (nums.length === 1) {
+        return nums[0]
+    }
+    for (let i=0; i < nums.length-1; i++) {
+        if (nums[i] === nums[i+1]){
+            counter += 2
+            if (counter > maxCounter) {
+                maxCounter = counter
+                maxNumber = nums[i]
+            }
+        } else {
+            counter = 0
+        }
+    }
+    return maxNumber
+};
