c4bb059776fcd941ff9fd838b23b43778cdc6768
jsots <justinsotolongo@gmail.com>
2023-06-08 11:02:16 -0400

    Solved but not optimized

---
+++ leetcode/Easy/majorityElement.js
@@ -0,0 +1,15 @@
+var majorityElement = function(nums) {
+    let numberCounter = {}
+    for (let i=0; i<nums.length; i++) {
+        numberCounter[nums[i]] ? numberCounter[nums[i]] ++ : numberCounter[nums[i]] = 1
+    }
+    let max = -Infinity
+    let majorityElement = 0
+    for (let key in numberCounter) {
+        if (numberCounter[key] > max) {
+            max = numberCounter[key]
+            majorityElement = key
+        }
+    }
+    return majorityElement
+};
