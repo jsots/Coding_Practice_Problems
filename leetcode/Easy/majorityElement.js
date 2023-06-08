f0755c40f452ec10b07910fa8b8229b032d53ab3
jsots <justinsotolongo@gmail.com>
2023-06-08 11:29:57 -0400

    Solved optimally

--- leetcode/Easy/majorityElement.js
+++ leetcode/Easy/majorityElement.js
@@ -66,3 +66,21 @@ var majorityElement = function(nums) {
     }
     return maxNumber
 };
+
+// Runtime - 98.16% and Memory - 98.43%
+
+const majorityElement = function(nums) {
+    let count = 0;
+
+    let candidate = null;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            candidate = nums[i];
+        }
+
+        count += nums[i] === candidate ? 1 : -1;
+    }
+
+    return candidate;
+};
