206ba7ef49ef765b20bafeb37a151e66cbb9caac
jsots <justinsotolongo@gmail.com>
2023-06-09 17:54:13 -0400

    First attempt

--- leetcode/Easy/missingNumber.js
+++ leetcode/Easy/missingNumber.js
@@ -1,3 +1,12 @@
 var missingNumber = function (nums) {
-    
+    nums.sort()
+    let start = nums[0]
+    let end = nums[nums.length-1]
+    for ( let i = start; i < end; i++) {
+        if (!nums.includes(i)) {
+            return i
+        }
+    }
 }
+
+console.log(missingNumber([3, 0, 1]))
