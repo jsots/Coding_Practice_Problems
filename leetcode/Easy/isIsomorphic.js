5271a3ad6f11ef8c4165e7a0a1e7b6141fce2948
jsots <justinsotolongo@gmail.com>
2023-06-12 12:01:04 -0400

    Added the solution but this will require further reading/studying

--- leetcode/Easy/isIsomorphic.js
+++ leetcode/Easy/isIsomorphic.js
@@ -19,3 +19,25 @@ var isIsomorphic = function(s, t) {
         return false
     }
 };
+
+// Solution discusses maps and sets. look into both of them
+
+var isIsomorphic = function(s, t) {
+    const map = new Map();
+    const set = new Set();
+    let n = s.length - 1;
+
+    while (n >= 0) {
+        if (map.has(s[n]) && map.get(s[n]) !== t[n]) {
+        return false;
+        }
+        if (!map.has(s[n]) && set.has(t[n])) {
+        return false;
+        }
+        set.add(t[n]);
+        map.set(s[n], t[n]);
+        n--;
+    }
+
+    return true;
+};
