df6bd536dcdda72bea7906ba89f986dd455f1638
jsots <justinsotolongo@gmail.com>
2023-06-12 11:28:46 -0400

    Added metrics

--- leetcode/Easy/fibonacci.js
+++ leetcode/Easy/fibonacci.js
@@ -1,6 +1,10 @@
+// Runtime = 94.95% and Memory - 87.4%
+
 var fib = function(n) {
     let fib = [1, 1]
-    if (n <= 2) {
+    if (n === 0) {
+        return 0
+    } else if (n <= 2) {
         return 1
     }
     for (let i = 1; i <= n; i++) {
