47066073826fff8d04defa14357601fbbb6afa1d
jsots <justinsotolongo@gmail.com>
2023-06-13 18:30:58 -0400

    close but no cigar

--- leetcode/Easy/checkStraightLine.js
+++ leetcode/Easy/checkStraightLine.js
@@ -1,9 +1,18 @@
 // Doesnt work for horizontal lines
 
 var checkStraightLine = function(coordinates) {
-    let m = (coordinates[0][1] - coordinates[1][1])/(coordinates[0][0]-coordinates[1][0])
+    let deltaY = (coordinates[0][1] - coordinates[1][1])
+    let deltaX = (coordinates[0][0]-coordinates[1][0])
+    let m = deltaY/deltaX
     let b = coordinates[0][1] - m*coordinates[0][0]
     for (let i = 2; i < coordinates.length; i++) {
+        if (deltaX === 0) {
+            if (coordinates[i][0] !== coordinates[coordinates.length-1][0]) {
+                return false
+            } else {
+                return true
+            }
+        }
         if (coordinates[i][1] !== (m*coordinates[i][0] + b)) {
             return false
         }
