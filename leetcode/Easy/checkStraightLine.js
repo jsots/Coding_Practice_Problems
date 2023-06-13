9d1b70b9cd70d1020b8872a9349fb2ef64109014
jsots <justinsotolongo@gmail.com>
2023-06-13 18:18:09 -0400

    cant believe this didnt work

--- leetcode/Easy/checkStraightLine.js
+++ leetcode/Easy/checkStraightLine.js
@@ -1,3 +1,11 @@
 var checkStraightLine = function(coordinates) {
     let m = (coordinates[0][1] - coordinates[1][1])/(coordinates[0][0]-coordinates[1][0])
+    let b = coordinates[0][1] - m*coordinates[0][0]
+    for (let i = 2; i < coordinates.length; i++) {
+        if (coordinates[i][1] !== (m*coordinates[i][0] + b)) {
+            return false
+        }
+    }
+    return true
+};
 };
