7073095db7c0831242dfcd56fa3f930c25e3033f
jsots <justinsotolongo@gmail.com>
2023-06-14 15:10:19 -0400

    First attempt

--- leetcode/Easy/pascalsTriangle.js
+++ leetcode/Easy/pascalsTriangle.js
@@ -1,10 +1,15 @@
 var generate = function(numRows) {
     let rowOne = [1]
+    let triangle = [[1]]
 
     for (let i = 2; i <= numRows; i++) {
+        let prevRow = rowOne
         let nextRow = new Array(i).fill(0)
         for (let j = 0; j < nextRow.length; j++) {
-            
+            nextRow[j] = prevRow[j-1] + prevRow[j]
+            triangle.push(nextRow)
+            prevRow = nextRow
         }
     }
+    return triangle
 };
