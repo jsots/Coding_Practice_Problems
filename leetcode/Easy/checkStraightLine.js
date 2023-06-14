d3381aa4a33c6eacc4fc614fd493ea51c494014a
jsots <justinsotolongo@gmail.com>
2023-06-14 13:59:42 -0400

    Finally solved it

--- leetcode/Easy/checkStraightLine.js
+++ leetcode/Easy/checkStraightLine.js
@@ -5,14 +5,16 @@ var checkStraightLine = function(coordinates) {
     let deltaX = (coordinates[0][0]-coordinates[1][0])
     let m = deltaY/deltaX
     let b = coordinates[0][1] - m*coordinates[0][0]
-    for (let i = 2; i < coordinates.length; i++) {
-        if (deltaX === 0) {
-            if (coordinates[i][0] !== coordinates[coordinates.length-1][0]) {
+
+    if (deltaX === 0) {
+        for (let j = 0; j < coordinates.length-1; j++) {
+            if (coordinates[j][0] !== coordinates[j+1][0]) {
                 return false
-            } else {
-                return true
-            }
+            } 
         }
+        return true
+    }
+    for (let i = 2; i < coordinates.length; i++) {
         if (coordinates[i][1] !== (m*coordinates[i][0] + b)) {
             return false
         }
