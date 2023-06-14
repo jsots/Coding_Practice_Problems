64e213817d64f17313c748a612817bf2f1a60b87
jsots <justinsotolongo@gmail.com>
2023-06-14 14:25:37 -0400

    better solution

--- leetcode/Easy/checkStraightLine.js
+++ leetcode/Easy/checkStraightLine.js
@@ -22,26 +22,19 @@ var checkStraightLine = function(coordinates) {
     return true
 };
 
-// solution below
-
+// better solution
 var checkStraightLine = function(coordinates) {
-    coordLen = coordinates.length
-
-    if(coordLen <= 2) return true
-
-    function calculateSlope(x1, y1, x2, y2, x, y) {
-    return (y2 - y1) * (x - x1) - (y - y1) * (x2 - x1);
-  }
-
-    let [x1, y1] = coordinates[0]
-    let [x2, y2] = coordinates[1]
-
-   for (let i = 2; i < coordinates.length; i++) {
-    let [x, y] = coordinates[i];
-    if (calculateSlope(x1, y1, x2, y2, x, y) !== 0) {
-      return false;
+    let grad;
+    for (let i = 0, j = 1; j < coordinates.length; i++, j++) {
+        let rise = coordinates[j][1] - coordinates[i][1];
+        let run = coordinates[j][0] - coordinates[i][0];
+        let currGrad;
+        if (run == 0) currGrad = Infinity;
+        else currGrad = rise / run;
+        if (i > 0) {
+            if (currGrad !== grad) return false;
+        }
+        grad = currGrad;
     }
-  }
-
-  return true;
+    return true;
 };
