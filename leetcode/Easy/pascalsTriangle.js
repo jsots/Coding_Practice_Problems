7db3e5236788ec7becd8d65f3da89012aa09c42b
jsots <justinsotolongo@gmail.com>
2023-06-14 14:53:22 -0400

    starting

---
+++ leetcode/Easy/pascalsTriangle.js
@@ -0,0 +1,10 @@
+var generate = function(numRows) {
+    let rowOne = [1]
+
+    for (let i = 2; i <= numRows; i++) {
+        let nextRow = new Array(i).fill(0)
+        for (let j = 0; j < nextRow.length; j++) {
+            
+        }
+    }
+};
