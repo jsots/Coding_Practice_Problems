5edb311363e0f0f4a5929aa31c99f2b55f778318
jsots <justinsotolongo@gmail.com>
2023-06-15 13:55:46 -0400

    Kinda solved but getting NaN'

--- leetcode/Easy/pascalsTriangle2.js
+++ leetcode/Easy/pascalsTriangle2.js
@@ -4,6 +4,16 @@ var getRow = function(rowIndex) {
     let triangle = []
 
     for (let i = 0; i <= rowIndex; i++) {
-        let row = new Array 
+        let row = new Array(i+1).fill(0)
+
+        row[0] = 1
+        row[i] = 1
+
+        for (let j=1; j < i; j++) {
+            row[j] = row[i-1][j-1] + row[i-1][j]
+        }
+
+        triangle.push(row)
     }
+    return triangle[triangle.length-1]
 };
