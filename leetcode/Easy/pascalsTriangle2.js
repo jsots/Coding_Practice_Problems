83a195241b49d8a383378698a3cf805e6df9ba6d
jsots <justinsotolongo@gmail.com>
2023-06-15 14:09:35 -0400

    Solved

--- leetcode/Easy/pascalsTriangle2.js
+++ leetcode/Easy/pascalsTriangle2.js
@@ -10,10 +10,10 @@ var getRow = function(rowIndex) {
         row[i] = 1
 
         for (let j=1; j < i; j++) {
-            row[j] = row[i-1][j-1] + row[i-1][j]
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         }
 
         triangle.push(row)
     }
-    return triangle[triangle.length-1]
+    return triangle[rowIndex]
 };
