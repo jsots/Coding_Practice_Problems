ff504fcff2b9e8b8110c84ca24b750fa76c2142c
jsots <justinsotolongo@gmail.com>
2023-06-26 18:34:23 -0400

    added new solution that is better

--- leetcode/Easy/findClosestNumber.js
+++ leetcode/Easy/findClosestNumber.js
@@ -13,3 +13,27 @@ var findClosestNumber = function(nums) {
 };
 
 console.log(findClosestNumber([-4,-2,1,4,8]))
+
+// Runtime - 68.53% and Memory 79.2% 
+
+var findClosestNumber = function(nums) {
+    let pos = Infinity;
+    let neg = -Infinity
+ 
+    for(let i=0; i<nums.length;i++) {
+        if(nums[i] > 0) {
+            if(pos > nums[i])
+            pos = nums[i]
+        } else {
+            if(neg < nums[i]) 
+             neg = nums[i]
+            }
+        
+    }
+ 
+        if(-neg < pos) {
+            return neg;
+        }
+        return pos;
+    
+ };
