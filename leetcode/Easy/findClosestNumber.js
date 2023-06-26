e1ac3be8d34bc1a3296c43e1ce5266594621e0a2
jsots <justinsotolongo@gmail.com>
2023-06-26 18:25:32 -0400

    solved

--- leetcode/Easy/findClosestNumber.js
+++ leetcode/Easy/findClosestNumber.js
@@ -1,12 +1,15 @@
 var findClosestNumber = function(nums) {
-    let shortestDistance
+    let shortestDistance = Infinity
+    let ans = 0
+    nums.sort()
     for (let i = 0; i < nums.length; i++) {
-        let currentDistance = nums[i]
-        if (currentDistance < shortestDistance) {
+        let currentDistance = Math.abs(nums[i])
+        if (currentDistance <= shortestDistance) {
             shortestDistance = currentDistance
+            ans = nums[i]
         }
     }
-    return shortestDistance
+    return ans
 };
 
 console.log(findClosestNumber([-4,-2,1,4,8]))
