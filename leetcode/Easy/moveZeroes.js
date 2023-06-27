bdf0a021a28abd410a32e8421d8ceaca716c8f0e
jsots <justinsotolongo@gmail.com>
2023-06-26 21:16:29 -0400

    moving one zero

--- leetcode/Easy/moveZeroes.js
+++ leetcode/Easy/moveZeroes.js
@@ -1,5 +1,14 @@
 var moveZeroes = function(nums) {
-    
+    let j = 0
+    for (let i = 0; i < nums.length-1; i++) {
+        if (nums[i] === 0) {
+            let temp = nums[i]
+            j++
+            nums[i] = nums[j]
+            nums[j] = temp
+        }
+    }
+    return nums
 };
 
 console.log(moveZeroes([0,1,0,3,12]))
