5bc5e3a2ff824313319c701465bd4b0570162b25
jsots <justinsotolongo@gmail.com>
2023-06-27 15:41:48 -0400

    attempted

--- leetcode/Easy/wordPattern.js
+++ leetcode/Easy/wordPattern.js
@@ -1,3 +1,15 @@
 var wordPattern = function(pattern, s) {
-    
+    let patternLetters = pattern.split("")
+    let sLetters = s.split(" ")
+    let dictionary = {}
+    if (patternLetters.length !== sLetters.length) {
+        return false
+    }
+    for (let i = 0; i < patternLetters.length; i++) {
+        if (dictionary[patternLetters[i]]) {
+            if (dictionary[patternLetters[i]])
+        }
+    }
 };
+
+console.log(wordPattern("abba", "dog cat cat dog"))
