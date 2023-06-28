a678db6cba5303c426485f2c6f3019bfec4ee79b
jsots <justinsotolongo@gmail.com>
2023-06-28 16:34:17 -0400

    added another solution

--- leetcode/Easy/containsDuplicate.py
+++ leetcode/Easy/containsDuplicate.py
@@ -10,3 +10,14 @@ class Solution:
             else:
                 unique_nums[nums[i]] = 1
         return False
+
+
+# Runtime - 7.60% and Memory - 93.25%
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
