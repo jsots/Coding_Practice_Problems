e230f4d09fbc20a2d90b2e65e80f38433908f6c5
jsots <justinsotolongo@gmail.com>
2023-06-28 16:10:43 -0400

    solved the valid parenthesis in python

---
+++ leetcode/Easy/validParenthesis.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        left = ["(", "[", "{"]
+        right = [")", "]", "}"]
+        for i in range(0, len(s)):
+            if s[i] in left:
+                stack.append(left.index(s[i]))
+            if s[i] in right:
+                if len(stack) == 0:
+                    return False
+                if stack[-1] == right.index(s[i]):
+                    del stack[-1]
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        return False
+            
