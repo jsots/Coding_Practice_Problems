c174848819131ac0ed5acb5fe381354cd74cdae1
jsots <justinsotolongo@gmail.com>
2023-06-30 19:56:36 -0400

    added static functions

--- sandbox/classes.py
+++ sandbox/classes.py
@@ -33,23 +33,65 @@ class Employee:
     
     def apply_raise(self):
         self.pay =  int(self.pay * self.raise_amount)
+
+    # This method will take the class as an instace. 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
     
+    # You can use class methods as an alternative constructor
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
+
+    # Static methods do not even pass in self. They are like functions.
+
+    @staticmethod
+    def is_workday(day):
+        # weekday is a method that prints 0-4 for mon-fri and 5 & 6 for sat & sun
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Test", "User", 60000)
 
-print(emp_1.fullname())
+# print(emp_1.fullname())
 # can alternatively do this with the class itself. pass in the emp_1 as the instance that gets passed into the method
-print(Employee.fullname(emp_2))
+# print(Employee.fullname(emp_2))
 
 # Below, all three will yield the same number. You can get it from the class itself, or from the instance of the class.
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# Now, notice what happens when you change the raise amount for just one employee.
+
+# emp_1.raise_amount = 1.05
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# Now, you can set the raise amount for all. 
+
+Employee.set_raise_amount(1.05)
+
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
 
-# Now, notice what happens when you change the raise amount for just one employee.
-emp_1.raise_amount = 1.05
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+
+import datetime
+my_date = datetime.date(2016, 7, 10) # This is a sunday, so false. 
+
+print(Employee.is_workday(my_date))
