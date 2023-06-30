7a9172726ef0b19a5a60ef021cac174eaee1dc3d
jsots <justinsotolongo@gmail.com>
2023-06-30 19:24:56 -0400

    adding variables

--- sandbox/classes.py
+++ sandbox/classes.py
@@ -14,6 +14,9 @@ user = Person(25, 80, 177, "Jon", "Snow", "You know nothing, Jon Snow")
 
 
 class Employee:
+    # class variable, something you would want applied to all employees
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -23,14 +26,26 @@ class Employee:
     # need to pass an instance, self, otherwise the methods will not work
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay =  int(self.pay * self.raise_amount)
+    
+
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Test", "User", 60000)
 
-
-print(emp_1.email)
-print(emp_2.email)
 print(emp_1.fullname())
-
 # can alternatively do this with the class itself. pass in the emp_1 as the instance that gets passed into the method
 print(Employee.fullname(emp_2))
+
+# Below, all three will yield the same number. You can get it from the class itself, or from the instance of the class.
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+# Now, notice what happens when you change the raise amount for just one employee.
+emp_1.raise_amount = 1.05
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
