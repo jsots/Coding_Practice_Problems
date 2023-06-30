c5cf09df14cc32d462dea6c6f5342bed5d2e2f46
jsots <justinsotolongo@gmail.com>
2023-06-30 19:06:03 -0400

    testing

--- sandbox/classes.py
+++ sandbox/classes.py
@@ -4,8 +4,21 @@ class Person:
         self.weight = weight
         self.height = height
         self.first_name = first_name
-        self.last_name - last_name
+        self.last_name = last_name
         self.catch_phrase = catch_phrase
 
 
-user = Person(25, 80, 177, "Jon", "Snow", "You know nothing, Jon SNow")
+user = Person(25, 80, 177, "Jon", "Snow", "You know nothing, Jon Snow")
+
+print(user.catch_phrase)
+
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
+
+emp_1 = Employee("Corey", "Schafer", 50000)
+emp_2 = Employee("Test", "User", 60000)
