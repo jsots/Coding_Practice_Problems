63f484745a25df67bbc1a4013a77af43689d06d7
jsots <justinsotolongo@gmail.com>
2023-07-03 10:48:35 -0400

    workin on it still lol

--- leetcode/Easy/distanceTraveled.py
+++ leetcode/Easy/distanceTraveled.py
@@ -1,16 +1,17 @@
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        counter = 0
+        consumed = 0
+        distance = 0
         if mainTank < 5:
             return mainTank * 10
         else:
-            if additionalTank >= 1:
-                while mainTank >= 5:
-                    mainTank - 5
-                    counter += 1
-                return ((mainTank + 1) * 10 * counter
-            else:
-                while mainTank >= 5:
-                    mainTank - 5
-                    counter += 1
-                return (mainTank * 10) * counter
+            while mainTank > 0:
+                consumed += 1
+                mainTank -= 1
+                distance += 10
+                if consumed == 5:
+                    consumed = 0
+                    if additionalTank >= 1:
+                        additionalTank -= 1
+                        distance += 10
+            return distance
