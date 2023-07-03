6551912270aa2a4335ffa63ca113b4b3e765dbad
jsots <justinsotolongo@gmail.com>
2023-07-03 03:22:55 -0400

    solved

--- leetcode/Easy/summaryRanges.py
+++ leetcode/Easy/summaryRanges.py
@@ -1,9 +1,20 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        frequency = {}
-        for num in nums:
-            if num in frequency:
-                frequency[num] += 1 
-            else:
-                frequency[num] = 0
-        return max(frequency, key=frequency.get)
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if len(nums) == 0:
+            return None
+        ans = []
+        start = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]+1:
+                end = nums[i-1]
+                if start == end:
+                    ans.append(str(start))
+                else:
+                    ans.append(str(start) + "->" + str(end))
+                start = nums[i]
+        end = nums[-1]
+        if start == end:
+            ans.append(str(start))
+        else:
+            ans.append(str(start) + "->" + str(end))
+        return ans
