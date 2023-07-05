05f199080ece9629b4c4f30e511f856233d93305
jsots <justinsotolongo@gmail.com>
2023-07-05 14:45:27 -0400

    solved two sum

--- leetcode/Easy/twoSum.py
+++ leetcode/Easy/twoSum.py
@@ -9,3 +9,16 @@ class Solution:
                     ans.append(i)
                     ans.append(j)
                     return ans
+
+
+# Solved again, on another day
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) < 2:
+            return None
+        for i in range(len(nums)):
+            for j in range (i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return None
