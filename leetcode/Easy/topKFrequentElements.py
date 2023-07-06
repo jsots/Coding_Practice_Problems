99e81724f7794c0bf19b35ebeaa95445849dfba3
jsots <justinsotolongo@gmail.com>
2023-07-06 14:06:22 -0400

    getting there

--- leetcode/Easy/topKFrequentElements.py
+++ leetcode/Easy/topKFrequentElements.py
@@ -1,10 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ans = {}
+        seen = {}
+        ans = []
         for num in nums:
             if num not in ans:
-                ans[num] = 1
+                seen[num] = 1
             else:
-                ans[num] += 1
+                seen[num] += 1
         for i in range(k):
-            
+            a = max(seen, key=seen.get)
+            ans.append(a)
+            seen.pop(a)
+        return ans
