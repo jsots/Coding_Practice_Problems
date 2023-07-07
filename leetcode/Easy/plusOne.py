f5bc45f19055cc6c8e987469f2b08e0922ee481f
jsots <justinsotolongo@gmail.com>
2023-07-07 12:20:07 -0400

    solved the plus one a  diff way

--- leetcode/Easy/plusOne.py
+++ leetcode/Easy/plusOne.py
@@ -8,3 +8,21 @@ class Solution:
         digit_joined = int("".join(digits_string)) + 1
         result = [int(digit) for digit in str(digit_joined)]
         return result
+
+# Solved below!!!
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        ans = []
+        carry = True
+        reverse = list(reversed(digits))
+        for i in range(len(digits)):
+            if reverse[i] == 9 and carry:
+                # do something
+                ans.append(0)
+            else:
+                ans.append(reverse[i] +1) if carry else ans.append(reverse[i])
+                carry = False
+        if carry:
+            ans.append(1)
+        return reversed(ans)
