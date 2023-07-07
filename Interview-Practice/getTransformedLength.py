7db9ffbb89caa759b7c2a3c44e84863a16808f7a
jsots <justinsotolongo@gmail.com>
2023-07-07 16:12:03 -0400

    transformed word

--- Interview-Practice/getTransformedLength.py
+++ Interview-Practice/getTransformedLength.py
@@ -1,13 +1,14 @@
 def getTransformedLength(word, t):
-    transformed = ""
     number_of_transformations = 0
     while number_of_transformations < t:
+        transformed = ""
         for char in word:
-            if char == z:
+            if char == "z":
                 transformed += "ab"
             else:
                 transformed += chr(ord(char) + 1)
         number_of_transformations += 1
+        word = transformed
     return len(transformed)
 
-print(getTransformedLength("defcz", 8))
+print(getTransformedLength("defczx", 3))
