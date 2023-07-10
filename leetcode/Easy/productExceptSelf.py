fe6bc725f526580711cf30078d70daf3ee7dc60d
jsots <justinsotolongo@gmail.com>
2023-07-10 10:54:25 -0400

    solved product except self

--- leetcode/Easy/productExceptSelf.py
+++ leetcode/Easy/productExceptSelf.py
@@ -1,18 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = []
-        total_product = 1
-        zero = False
-        for num in nums:
-            if num != 0:
-                total_product *= num
-            if num == 0:
-                zero = True
-        for num in nums:
-            if num != 0 and zero:
-                ans.append(0)
-            if num != 0 and not zero:
-                ans.append(total_product//num)
-            if num == 0 and zero:
-                ans.append(total_product)
+        ans = [1] * len(nums)
+        prefix = 1
+        postfix = 1
+        for i in range(len(nums)):
+            ans[i] *= prefix
+            prefix *= nums[i]
+        for j in range(len(nums)-1, -1, -1):
+            ans[j] *= postfix
+            postfix *= nums[j]
+            
         return ans
