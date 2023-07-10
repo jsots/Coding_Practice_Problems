ea2df0a1810e9181daf82b3f8f73d01a37f1f2fb
jsots <justinsotolongo@gmail.com>
2023-07-10 15:20:27 -0400

    solved with clean version

--- leetcode/medium/evalRPN.py
+++ leetcode/medium/evalRPN.py
@@ -19,3 +19,26 @@ class Solution:
                 solution_stack.append(int(token))
         
         return solution_stack[0]
+
+
+# cleaner version below:
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        solution_stack = []
+        for token in tokens:
+            if token == "+":
+                solution_stack.append(solution_stack.pop() + solution_stack.pop())
+            elif token == "*":
+                solution_stack.append(solution_stack.pop() * solution_stack.pop())
+            elif token == "-":
+                b = solution_stack.pop()
+                a = solution_stack.pop()
+                solution_stack.append(a - b)
+            elif token == "/":
+                b = solution_stack.pop()
+                a = solution_stack.pop()
+                solution_stack.append(int(a / b))
+            else:
+                solution_stack.append(int(token))
+        
+        return solution_stack[0]
