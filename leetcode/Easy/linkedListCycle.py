0101747e5ccf388924e2788504434dfbc0b181a3
jsots <justinsotolongo@gmail.com>
2023-07-11 11:58:36 -0400

    added another approach

--- leetcode/Easy/linkedListCycle.py
+++ leetcode/Easy/linkedListCycle.py
@@ -19,3 +19,25 @@ class Solution:
             head = nxt
         
         return False
+
+
+# Below is the tortoise and hare approach:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Are all the values unique?
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
