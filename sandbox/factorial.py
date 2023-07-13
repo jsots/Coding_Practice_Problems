00dcdb0d4c7edccb8d79f213f5ffc2a283dd6b2b
jsots <justinsotolongo@gmail.com>
2023-07-13 09:51:28 -0400

    added the nonrecursive version as well

--- sandbox/factorial.py
+++ sandbox/factorial.py
@@ -5,9 +5,20 @@ def factorial(num):
     return num * factorial(num-1) 
 
 # O(n) for time
-# O(1) for space
+# O(n) for space because of the call stack
+
+def factorial_nonrecursive(num):
+    res = 1
+    while num > 1:
+        res *= num
+        num -= 1
+    return res
 
 
 print(factorial(3))
 print(factorial(4))
 print(factorial(5))
+
+print(factorial_nonrecursive(3))
+print(factorial_nonrecursive(4))
+print(factorial_nonrecursive(5))
