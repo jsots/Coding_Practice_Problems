3e9b883606b3bdfa1f62d1185ed5346b6550d900
jsots <justinsotolongo@gmail.com>
2023-07-15 11:05:53 -0400

    trying 3sum again

--- leetcode/medium/3sum.py
+++ leetcode/medium/3sum.py
@@ -19,3 +19,25 @@ class Solution:
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return ans
+
+# Attempting again. I know im close!
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        # sort to start, that way it will be two sum 2 moving forward
+        for i, a in enumerate(nums):
+            j = i+1
+            k = len(nums) - 1
+            while j < k:
+                need = -a
+                if nums[j] + nums[k] < need:
+                    j += 1
+                elif nums[j] + nums[k] > need:
+                    k -= 1
+                else:
+                    if [nums[i], nums[j], nums[k]] not in ans:
+                        ans.append([nums[i], nums[j], nums[k]])
+                    break
+        return ans
