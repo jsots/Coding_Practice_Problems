8e5d52282a7f7bff89c7216296a2ab12da569c51
jsots <justinsotolongo@gmail.com>
2023-07-18 11:08:27 -0400

    this is the code for inserting

---
+++ sandbox/BSTInsertandRemove.py
@@ -0,0 +1,11 @@
+def insert(root, val):
+    # Base case, root is null/None
+    if not root:
+        return TreeNode(val)
+
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    
+    return root
