e038f6eb528e33e2d83ec9dc3bde4a30a7e7b5d1
jsots <justinsotolongo@gmail.com>
2023-07-18 16:04:57 -0400

    adding the path

--- sandbox/canReachLeaf_Backtracking.py
+++ sandbox/canReachLeaf_Backtracking.py
@@ -10,3 +10,19 @@ def canReachLeaf(root):
         return True
     
     return False
+
+
+def leafPath(root):
+    if not root or root.val == 0:
+        return False
+    path.append(root.val)
+    
+    if not root.left and not root.right:
+        return True 
+    if canReachLeaf(root.left):
+        return True
+    if canReachLeaf(root.right):
+        return True
+    path.pop()
+    
+    return False
