fde680b1b5f2fa8ca784040cbe327f82d429c47a
jsots <justinsotolongo@gmail.com>
2023-07-18 15:38:20 -0400

    right side solved

--- leetcode/medium/rightSideViewBST.py
+++ leetcode/medium/rightSideViewBST.py
@@ -1,3 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         q = collections.deque()
@@ -6,9 +12,15 @@ class Solution:
             q.append(root)
 
         while q:
-            curr = q.popleft()
-            ans.append(curr.val)
-            if curr.right:
-                q.append(curr.right)
-        
+            right_side = None
+            q_len = len(q)
+            for i in range(q_len):
+                node = q.popleft()
+                if node:
+                    right_side = node
+                    # add the left first, that way the right side will always be the last thing added to the q.
+                    q.append(node.left)
+                    q.append(node.right)
+            if right_side:
+                ans.append(right_side.val)
         return ans
