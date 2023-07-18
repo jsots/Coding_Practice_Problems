03d7bfa0de5dd1786c3766f41a371b96db0351da
jsots <justinsotolongo@gmail.com>
2023-07-18 16:05:32 -0400

    added the right inputs

--- sandbox/canReachLeaf_Backtracking.py
+++ sandbox/canReachLeaf_Backtracking.py
@@ -12,16 +12,16 @@ def canReachLeaf(root):
     return False
 
 
-def leafPath(root):
+def leafPath(root, path):
     if not root or root.val == 0:
         return False
     path.append(root.val)
     
     if not root.left and not root.right:
         return True 
-    if canReachLeaf(root.left):
+    if canReachLeaf(root.left, path):
         return True
-    if canReachLeaf(root.right):
+    if canReachLeaf(root.right, path):
         return True
     path.pop()
     
