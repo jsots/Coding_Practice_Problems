9e68a016e7a7948acbd71137f6519f087386216e
jsots <justinsotolongo@gmail.com>
2023-07-19 09:32:44 -0400

    solved the 2d matrix binary search

--- leetcode/medium/search2DMatrix.py
+++ leetcode/medium/search2DMatrix.py
@@ -34,3 +34,37 @@ class Solution:
         if matrix[left_m][left_n] == target:
             return True
         return False
+
+
+
+# Solved on a new day:
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix) - 1, len(matrix[0]) -1
+        top, bot = 0, rows
+
+        while top <= bot:
+            mid = (top + bot) // 2
+            if target > matrix[mid][-1]:
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bot = mid - 1
+            else:
+                break
+
+        row = (top + bot) // 2
+        l, r = 0, len(matrix[0]) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target > matrix[row][mid]:
+                l = mid + 1
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            else:
+                return True
+        
+        return False
+
+    
