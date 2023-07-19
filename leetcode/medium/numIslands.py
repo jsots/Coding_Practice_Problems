87003e69dffab3a1592973a7de4f90551db9fae1
jsots <justinsotolongo@gmail.com>
2023-07-19 14:35:07 -0400

    added the other way to solve it

--- leetcode/medium/numIslands.py
+++ leetcode/medium/numIslands.py
@@ -23,3 +23,31 @@ class Solution:
                     islands += 1
         
         return islands
+
+
+# same as above but changing it to v for visit instead of making a hash map
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1":
+                return
+            grid[r][c] = "v"
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c):
+                    dfs(r, c)
+                    islands += 1
+        
+        return islands
