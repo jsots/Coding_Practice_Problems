415bc6bf0e13330f8fd887f0da18c2129a0cc756
jsots <justinsotolongo@gmail.com>
2023-07-21 08:17:37 -0500

    invert tree but very concise

--- leetcode/Easy/invertTree.py
+++ leetcode/Easy/invertTree.py
@@ -11,3 +11,21 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+
+        
