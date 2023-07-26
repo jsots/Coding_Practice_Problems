120f02a64c393b47de831221fbc773e26229382f
jsots <justinsotolongo@gmail.com>
2023-07-26 15:17:07 -0400

    sweets and savories done a diff way

--- Algo-Expert/Blue/sweetAndSavory.py
+++ Algo-Expert/Blue/sweetAndSavory.py
@@ -25,3 +25,37 @@ def sweetAndSavory(dishes, target):
             break
         
     return [best_l, best_r]
+
+
+
+# Another way to solve, using two separate arrays.
+
+def sweetAndSavory(dishes, target):
+    # Write your code here.
+    sweets = [dish for dish in dishes if dish < 0]
+    savories = [dish for dish in dishes if dish > 0]
+    sweets.sort()
+    savories.sort(reverse = True)
+
+    print(savories)
+
+    best_pair = [0, 0]
+    best_diff = float("inf")
+    sweet_idx, savory_idx = 0, 0
+
+    if not sweets or not savories:
+        return best_pair
+
+    while sweet_idx < len(sweets) and savory_idx < len(savories):
+        cur = sweets[sweet_idx] + savories[savory_idx]
+
+        if cur <= target:
+            if abs(target - cur) < best_diff:
+                best_diff = abs(target - cur)
+                best_pair = [sweets[sweet_idx], savories[savory_idx]]
+            sweet_idx += 1
+        else:
+            savory_idx += 1
+            
+    return best_pair
+
