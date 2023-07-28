04c4e5e05fe90e1cc4af854c7c39fd3c6a0ec0a1
jsots <justinsotolongo@gmail.com>
2023-07-28 12:29:41 -0400

    solved

--- leetcode/Easy/rangeSumBST.py
+++ leetcode/Easy/rangeSumBST.py
@@ -28,3 +28,35 @@ class Solution:
         
         return cur_sum
 
+
+# Soved below, my own way:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        ans = []
+        cur_sum = 0
+        def dfs(node):
+            if not node:
+                return
+            
+            if node.val >= low and node.val <= high:
+                ans.append(node.val)
+
+            dfs(node.left)
+            dfs(node.right)
+
+            return ans
+        
+        dfs(root)
+        for num in ans:
+            cur_sum += num
+        
+        return cur_sum
+
+
