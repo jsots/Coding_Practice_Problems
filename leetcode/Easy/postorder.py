22b8dc721757e420daad14ecea7391257a5e0205
jsots <justinsotolongo@gmail.com>
2023-07-28 12:47:06 -0400

    post order with many children

---
+++ leetcode/Easy/postorder.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(node):
+            if not node:
+                return
+            
+            if node.children:
+                for child in node.children:
+                    dfs(child)
+    
+            ans.append(node.val)
+
+            return ans
+        
+        return dfs(root)
