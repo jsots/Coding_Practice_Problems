26a95a9667fea0a45e2b26f29dcfb855ca2beeb7
jsots <justinsotolongo@gmail.com>
2023-08-04 13:09:45 -0400

    reversed words

---
+++ leetcode/medium/reverseWords.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        i = 0
+        words = []
+        while i < len(s):
+            while i < len(s) and s[i] == " ":
+                i += 1
+            
+            if i < len(s):
+                word = []
+                while i < len(s) and s[i] != " ":
+                    word.append(s[i])
+                    i += 1
+                words.append("".join(word))
+                i += 1  # Skip over the space after the word if it exists
+        
+        return " ".join(reversed(words))
