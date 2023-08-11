9521aaf847ff7af04e8572233a1a9c8145168ead
jsots <justinsotolongo@gmail.com>
2023-08-11 13:50:32 -0400

    learnin

--- sandbox/introduction.ts
+++ sandbox/introduction.ts
@@ -1,2 +1,12 @@
 const a = [];
 // This is not an array 
+
+function sum_char(n: string): number {
+    let sum = 0;
+    for (let i = 0; i < n.length; ++i) {
+        for (let j = 0; j < n.length; ++j) {
+            sum += charCode;
+        }
+    }
+    return sum
+}
