138735067d62b0827f020cc2a9d71b18d95ee269
jsots <justinsotolongo@gmail.com>
2023-08-15 13:11:03 -0400

    needle and haystack in typescript

--- sandbox/introduction.ts
+++ sandbox/introduction.ts
@@ -1,12 +1,24 @@
 const a = [];
 // This is not an array 
 
-function sum_char(n: string): number {
-    let sum = 0;
-    for (let i = 0; i < n.length; ++i) {
-        for (let j = 0; j < n.length; ++j) {
-            sum += charCode;
+// function sum_char(n: string): number {
+//     let sum = 0;
+//     for (let i = 0; i < n.length; ++i) {
+//         for (let j = 0; j < n.length; ++j) {
+//             sum += charCode;
+//         }
+//     }
+//     return sum
+// }
+
+
+export default function linear_search(haystack: number[], needle: number): boolean {
+
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
         }
     }
-    return sum
+
+    return false;
 }
