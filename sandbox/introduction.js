18a04d700f03cfdccd73e86dcb7b8b53c242149f
jsots <justinsotolongo@gmail.com>
2023-08-15 13:18:50 -0400

    gettin started with making functions in ts

--- sandbox/introduction.js
+++ sandbox/introduction.js
@@ -1,6 +1,7 @@
+"use strict";
 // const a = [];
 // This is not an array 
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // function sum_char(n: string): number {
 //     let sum = 0;
 //     for (let i = 0; i < n.length; ++i) {
@@ -10,16 +11,13 @@
 //     }
 //     return sum
 // }
-
-
-export default function linear_search(haystack: number[], needle: number): boolean {
-
-    for (let i = 0; i < haystack.length; ++i) {
+function linear_search(haystack, needle) {
+    for (var i = 0; i < haystack.length; ++i) {
         if (haystack[i] === needle) {
             return true;
         }
     }
-
     return false;
 }
-console.log(linear_search([1,2,3], 2))
+exports.default = linear_search;
+console.log(linear_search([1, 2, 3], 2));
