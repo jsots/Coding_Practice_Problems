87feabea2aa3b7080e0f4333c486d1e4f39e1a5e
jsots <justinsotolongo@gmail.com>
2023-08-16 11:12:23 -0400

    find max average

--- leetcode/Easy/findMaxAverage.py
+++ leetcode/Easy/findMaxAverage.py
@@ -1,9 +1,13 @@
-def findMaxAverage(self, nums: List[int], k: int) -> float:
-        current_sum = sum(nums[:k])  # Calculate initial sum of the first k elements
-        max_avg = current_sum / k  # Initialize max_avg with the average of the first k elements
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        cur_sum = sum(nums[:k])
+        cur_avg = cur_sum/k
+        max_avg = cur_avg
 
         for i in range(k, len(nums)):
-            current_sum += nums[i] - nums[i - k]  # Update the current sum by subtracting the leftmost element and adding the rightmost element
-            max_avg = max(max_avg, current_sum / k)  # Update the max_avg if a higher average is found
+            cur_sum += nums[i] - nums[i - k]
+            cur_avg = cur_sum/k
+            max_avg = max(cur_avg, max_avg)
 
         return max_avg
+
