a51d42d01062d90da8c912f29902ea5c2ee98edb
jsots <justinsotolongo@gmail.com>
2023-08-23 17:19:11 -0400

    solved w chat gpt

--- leetcode/medium/increasingTriplet.py
+++ leetcode/medium/increasingTriplet.py
@@ -1,12 +1,14 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        for i in range(0, len(nums) - 2):
-            first = nums[i]
-            for j in range(i + 1, len(nums) - 1):
-                if nums[j] > first:
-                    second = nums[j]
-                    for k in range(j + 1, len(nums)):
-                        if nums[k] > second:
-                            return True
-
+        first = float('inf')
+        second = float('inf')
+        
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+                
         return False
