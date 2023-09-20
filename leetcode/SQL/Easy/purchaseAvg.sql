cbd9b10ef22b3e6912e7c054b0b06debb271a867
jsots <justinsotolongo@gmail.com>
2023-09-20 14:10:08 -0400

    solved and fixed for edge cases

---
+++ leetcode/SQL/Easy/purchaseAvg.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, if(SUM(units) is not null, ROUND(SUM(p.price * u.units) / SUM(u.units), 2),0) AS 'average_price'
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.Start_date AND p.end_date
+GROUP BY p.product_id
+ORDER BY p.product_id;
