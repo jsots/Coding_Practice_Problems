dafbacbd3c6cef363a3842d039ab28193d4817cb
jsots <justinsotolongo@gmail.com>
2023-09-20 14:35:12 -0400

    solved

---
+++ leetcode/SQL/Easy/percentageOfPorjects.sql
@@ -0,0 +1,8 @@
+SELECT r.contest_id, ROUND(COUNT(r.user_id) / total_users * 100, 2) AS 'percentage'
+FROM Register r
+JOIN (
+  SELECT COUNT(*) AS total_users
+  FROM Users
+) AS u_total
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
