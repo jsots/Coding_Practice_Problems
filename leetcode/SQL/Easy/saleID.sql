65bec180734880675f0a74c3ef674759e61fcee7
jsots <justinsotolongo@gmail.com>
2023-09-20 11:48:38 -0400

    need to lookover this again

--- leetcode/SQL/Easy/saleID.sql
+++ leetcode/SQL/Easy/saleID.sql
@@ -1,1 +1,6 @@
-SELECT product_name, year, price FROM Product p JOIN Sales s ON p.product_id = s.product_id ORDER BY p.product_name desc;
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans 
+from Visits v 
+LEFT JOIN Transactions t 
+ON v.visit_id = t.visit_id  
+WHERE t.transaction_id IS NULL 
+GROUP BY v.customer_id; 
