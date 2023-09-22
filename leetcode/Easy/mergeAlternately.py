df3fcdc1862b64c9a9f25cd9da5824ea3c822f5a
jsots <justinsotolongo@gmail.com>
2023-09-22 16:14:16 -0400

    merge alternatively done a new way

--- leetcode/Easy/mergeAlternately.py
+++ leetcode/Easy/mergeAlternately.py
@@ -13,3 +13,18 @@ class Solution:
             ans.append(word2[i+1:])
         
         return "".join(ans)
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = []
+        if len(word1) > len(word2):
+            for i in range(len(word2)):
+                ans.append(word1[i])
+                ans.append(word2[i])
+            return "".join(ans) + word1[i+1:]
+        else:
+            for i in range(len(word1)):
+                ans.append(word1[i])
+                ans.append(word2[i])
+            return "".join(ans) + word2[i+1:]
