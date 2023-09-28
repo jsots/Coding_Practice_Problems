ad7df535e3caeaf18d1ea60275e89ebd67368f77
jsots <justinsotolongo@gmail.com>
2023-09-28 15:30:35 -0400

    solved valid anagram

--- leetcode/Easy/validAnagram.py
+++ leetcode/Easy/validAnagram.py
@@ -69,3 +69,24 @@ class Solution:
                 return False
         
         return True
+
+
+# solved another day
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+
+        if len(s) != len(t):
+            return False
+
+        for char in s:
+            s_map[char] = s_map.get(char, 0) + 1
+        
+        for char in t:
+            if char not in s_map or s_map[char] == 0:
+                return False
+            s_map[char] -= 1
+        
+        
+        return True
