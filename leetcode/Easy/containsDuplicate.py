ca1d8ba3e964cf49c116a85f975c917fd5c1f9ca
jsots <justinsotolongo@gmail.com>
2023-09-28 15:24:25 -0400

    seen with has set

--- leetcode/Easy/containsDuplicate.py
+++ leetcode/Easy/containsDuplicate.py
@@ -45,3 +45,15 @@ class Solution:
             else:
                 return True
         return False
+
+
+# solved another day
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        
+        return False
