84a6603072095088c6bf1ac72394ecf1cad96ad5
jsots <justinsotolongo@gmail.com>
2023-09-28 16:13:41 -0400

    solved

--- sandbox/defaultDict.py
+++ sandbox/defaultDict.py
@@ -53,3 +53,24 @@ for name in names:
 # Print the indexed names
 for letter, name_list in name_index.items():
     print(f"Names starting with '{letter}': {', '.join(name_list)}")
+
+from collections import defaultdict
+
+# Define a factory function to return an empty list
+def default_list():
+    return []
+
+# List of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+number_groups = defaultdict(default_list)
+
+# Your code here
+for num in numbers:
+    if num % 2:
+        number_groups["odd"].append(num)
+    else:
+        number_groups["even"].append(num)
+
+# Print the grouped numbers
+print("Even numbers:", number_groups["even"])
+print("Odd numbers:", number_groups["odd"])
