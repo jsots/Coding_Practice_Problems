d3238df5f04393d71c633275e0100d574b98cf31
jsots <justinsotolongo@gmail.com>
2023-09-28 14:22:50 -0400

    binary search in typescript

--- sandbox/linearSearchAndKata.ts
+++ sandbox/linearSearchAndKata.ts
@@ -1,7 +1,27 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+export function linear_search(haystack: number[], needle: number): boolean {
     for (let i = 0; i < haystack.length; ++i) {
         if (haystack[i] === needle) {
             return true
         }
     }
+}
+
+export function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m +1;
+        }
+
+    } while (lo < hi);
+
+    return false;
 }
