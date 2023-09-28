999ae7fc73c07d71097d947ff4f994af8f797ed1
jsots <justinsotolongo@gmail.com>
2023-09-28 15:02:14 -0400

    bubble sort

--- sandbox/linearSearchAndKata.ts
+++ sandbox/linearSearchAndKata.ts
@@ -47,3 +47,17 @@ export function two_crystal_balls(breaks: boolean[]): number {
 
     return -1;
 }
+
+
+export bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
+
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
