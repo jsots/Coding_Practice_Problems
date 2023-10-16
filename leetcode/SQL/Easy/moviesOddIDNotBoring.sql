2418dd8600bdd40932c4581e8a338af36307786e
jsots <justinsotolongo@gmail.com>
2023-10-16 13:02:22 -0400

    did the movies one again

--- leetcode/SQL/Easy/moviesOddIDNotBoring.sql
+++ leetcode/SQL/Easy/moviesOddIDNotBoring.sql
@@ -2,3 +2,9 @@ SELECT *
 FROM Cinema
 WHERE id % 2 = 1 AND description != 'boring'
 ORDER BY rating desc;
+
+
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1 AND description NOT LIKE 'boring'
+ORDER BY rating desc;
