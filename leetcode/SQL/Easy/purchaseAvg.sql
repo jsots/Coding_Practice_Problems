5a3b895498d09751b3b8870158471af204ba609e
jsots <justinsotolongo@gmail.com>
2023-10-16 13:50:48 -0400

    solved the average price again

--- leetcode/SQL/Easy/purchaseAvg.sql
+++ leetcode/SQL/Easy/purchaseAvg.sql
@@ -4,3 +4,10 @@ LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.Start_date AND p.end_date
 GROUP BY p.product_id
 ORDER BY p.product_id;
+
+
+SELECT p.product_id, ROUND(IFNULL(SUM(p.price * u.units)/ SUM(units), 0), 2) as average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON u.product_id = p.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
