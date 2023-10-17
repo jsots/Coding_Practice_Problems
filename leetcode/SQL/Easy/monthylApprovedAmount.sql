a45e4acf59dfb3b2757fcfd87e73f770d6f51b50
jsots <justinsotolongo@gmail.com>
2023-10-17 16:29:44 -0400

    approved amount

---
+++ leetcode/SQL/Easy/monthylApprovedAmount.sql
@@ -0,0 +1,3 @@
+SELECT DATE_FORMAT(trans_date, '%Y-%m') as month, country, COUNT(trans_date) as trans_count, COUNT(CASE WHEN state LIKE 'approved' THEN trans_date END) as approved_count, SUM(amount) as trans_total_amount, SUM(CASE WHEN state LIKE 'approved' THEN amount WHEN state NOT LIKE 'approved' THEN 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY country, month
