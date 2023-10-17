fb64bf312f9264970bddbc4cb3a5347ecf4286a9
jsots <justinsotolongo@gmail.com>
2023-10-17 13:20:30 -0400

    total users

--- leetcode/SQL/Easy/percentageOfPorjects.sql
+++ leetcode/SQL/Easy/percentageOfPorjects.sql
@@ -6,3 +6,15 @@ JOIN (
 ) AS u_total
 GROUP BY r.contest_id
 ORDER BY percentage DESC, r.contest_id;
+
+-- Redone with CTES:
+
+WITH total_users (total) as (
+  SELECT COUNT(*) as total
+  FROM users
+)
+
+SELECT r.contest_id, ROUND(COUNT(r.user_id) / (SELECT total FROM total_users) * 100, 2) AS 'percentage'
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
