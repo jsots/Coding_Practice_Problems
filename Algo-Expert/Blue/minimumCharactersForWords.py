e45ba20378c8554ade95b09e9665279c4c22feb0
jsots <justinsotolongo@gmail.com>
2024-01-31 11:50:52 -0500

    close to the solution

---
+++ Algo-Expert/Blue/minimumCharactersForWords.py
@@ -0,0 +1,28 @@
+def minimumCharactersForWords(words):
+    # Write your code here.
+    # iterate through the array
+    # iterate through the letters in the string
+    # store them, probably in a hashmap
+    # i want to compare individual key totals, and replace with the highest
+    # produce the values in the set as an array
+
+    # do charactersw appear twice?
+    # do i include punctiuation marks?
+    # capital letters? 
+    
+    master_char = {}
+    ans = []
+    
+    for word in words:
+        current_char = {}
+        for char in word:
+            current_char[char] = current_char.get(char, 0) + 1 # consulted chat
+        for key, value in current_char.items():
+            master_char[key] = max(master_char.get(char, 0), value)
+
+    ans = [] 
+    # consulted chat
+    for key, value in master_char.items():
+        ans.extend([key] * value)
+
+    return ans
