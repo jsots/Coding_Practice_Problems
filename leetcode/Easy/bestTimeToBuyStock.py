8699f5e4beb0fde358d14ec656b71358808304a4
jsots <justinsotolongo@gmail.com>
2024-02-15 11:42:44 -0500

    solved now need to optimize a bit

--- leetcode/Easy/bestTimeToBuyStock.py
+++ leetcode/Easy/bestTimeToBuyStock.py
@@ -13,14 +13,37 @@ class Solution:
         return max_profit
 
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # keep track of max profit as a variable
+        # buy day is one variable
+        # sell day is another variable
+        # if buy day less than sell day, compare to current max profit and keep highest value
+        # then, move only the sell day to get highest profit
+        # if the sell day is less than buy day, then that becomes the new buy day
+        # return max profit
+        # Questions:
+        # What if there are two opportunities to yield a max profit?
+        buy_day = 0
+        sell_day = 1
+        max_profit = 0
+
+        while sell_day < len(prices):
+            buy = prices[buy_day]
+            sell = prices[sell_day]
+
+            if buy > sell:
+                buy_day = sell_day
+                sell_day += 1
+            else:
+                max_profit = max(max_profit, sell - buy)
+                sell_day += 1
         
-        
-        return 
-
-
-
+        return max_profit
 
 
 
