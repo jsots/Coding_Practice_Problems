ca90e0369541fd7a8062f4ff2f5e976b35d23e1c
jsots <justinsotolongo@gmail.com>
2024-03-04 14:05:08 -0500

    working on a plan

--- Algo-Expert/Blue/spiralTraverse.py
+++ Algo-Expert/Blue/spiralTraverse.py
@@ -4,6 +4,7 @@
 # Is empty a valid input?
 
 # Iterate until all cells have been visited. Maybe keep track of this with a set?
+# Could also keep track of max steps.
 # Iterate through the row first, stopping at the end. Then, go down the column, then go backwards, then vert backwards, then reset. This will be repeated until completion. Reg, reg, reverse, reverse. 
 
 # Keep track of answer in an array. Append the array as I visit each spot. 
@@ -12,7 +13,9 @@ def spiralTraverse(array):
     # Write your code here.
     r_min, r_max = 0, len(array)
     c_min, c_max = 0, len(array[0])
+    max_steps = (len(array) + 1) * (len(array[0]) + 1)
 
     spiral_traverse = []
-    
+    for r in range(r_max):
+
     pass
